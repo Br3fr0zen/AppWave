a4062c18f7501cfbc13de8eb6d96a170e7e8a707
EnriqueDAM <Enrique@10.34.80.123>
2016-01-20 20:12:08 +0100

    Version kike, no la piseis y mañana vemos lo de las ramas

--- app/src/main/java/com/javierbravo/yep/SignUpActivity.java
+++ app/src/main/java/com/javierbravo/yep/SignUpActivity.java
@@ -1,15 +1,27 @@
 package com.javierbravo.yep;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import com.parse.ParseObject;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class SignUpActivity extends AppCompatActivity {
 
-    private int score;
-    private int playerName;
-    private int cheatMode;
+    private EditText username;
+    private EditText password;
+    private EditText email;
+    private String usr = "";
+    private String pass = "";
+    private String em = "";
+    protected TextView mLogInTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,12 +29,42 @@ public class SignUpActivity extends AppCompatActivity {
 
         getSupportActionBar().hide();
 
-        ParseObject user = new ParseObject("Users");
+        username = (EditText) findViewById(R.id.usernameField);
+        password = (EditText) findViewById(R.id.passwordField);
+        email = (EditText) findViewById(R.id.emailField);
 
-        user.put("name", "Manu");
-        user.put("password", "1234");
-        user.put("email", "magno@hotmilio.com");
+        usr = username.getText().toString();
+        pass = password.getText().toString();
+        em = email.getText().toString();
 
-        user.saveInBackground();
+        mLogInTextView = (TextView) findViewById(R.id.signupButton);
+        mLogInTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signUpClick();
+            }
+        });
+    }
+
+    protected void signUpClick(){
+        ParseUser newUser = new ParseUser();
+
+        newUser.setUsername(usr);
+        newUser.setPassword(String.valueOf(pass));
+        newUser.setEmail(String.valueOf(em));
+
+        newUser.signUpInBackground(new SignUpCallback() {
+            public void done(ParseException e) {
+                if (e == null) {
+                    Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+                    startActivity(intent);
+                } else {
+                    Toast toast2 =
+                            Toast.makeText(getApplicationContext(),
+                                    "Username or email already in use, please introduce a new one", Toast.LENGTH_SHORT);
+                    toast2.show();
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/javierbravo/yep/WaveApplication.java
+++ app/src/main/java/com/javierbravo/yep/WaveApplication.java
@@ -7,14 +7,11 @@ import com.parse.Parse;
 /**
  * Created by Javier Bravo on 14/01/2016.
  */
-public class YepApplication extends Application {
+public class WaveApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
-        // [Optional] Power your app with Local Datastore. For more info, go to
-        // https://parse.com/docs/android/guide#local-datastore
-        // Parse.enableLocalDatastore(this);
         Parse.initialize(this);
     }
 }
